d33b4a7f2d35b3911e8df00304263521151cce1d
Ashvin Sunga <ashvinsunga@yahooo.com.ph>
2022-06-19 21:31:16 +0800

    technologies done

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -1,5 +1,6 @@
 import React, { useEffect } from "react";
 import Layout from "../components/Layout";
+import projectsData from "../resources/projects";
 import { TbBrandNextjs } from "react-icons/tb";
 import { DiNodejs } from "react-icons/di";
 import { SiGmail } from "react-icons/si";
@@ -121,44 +122,44 @@ function Home() {
         {/* Technologies */}
         <div className="mt-20">
           <h1
-            className="text-4xl md:text-2xl text-blue-800 font-bold text-center my-8"
+            className="text-4xl md:text-2xl text-blue-800 font-bold text-center"
             data-aos="slide-up"
           >
             Technologies I've used so far
           </h1>
           <div
-            className="grid md:grid-cols-1 grid-cols-6 "
+            className="grid md:grid-cols-1 grid-cols-6 content-start"
             data-aos="slide-left"
           >
             <FaReact
-              size={150}
+              size={120}
               color="cyan"
-              className="w-full text-center mt-20  "
+              className="w-full text-center mt-10"
             />
             <SiMongodb
-              size={150}
+              size={120}
               color="green"
-              className="w-full text-center mt-20  "
+              className="w-full text-center mt-10  "
             />
             <TbBrandNextjs
-              size={150}
+              size={120}
               color="black"
-              className="w-full bg-white text-center mt-20"
+              className="w-full bg-white text-center mt-10"
             />
             <DiNodejs
-              size={150}
+              size={120}
               color="green"
-              className="w-full text-center mt-20  "
+              className="w-full text-center mt-10"
             />
             <FaBootstrap
-              size={150}
+              size={120}
               color="#602C50"
-              className="w-full text-center mt-20    "
+              className="w-full text-center mt-10"
             />
             <FaJsSquare
-              size={150}
+              size={120}
               color="#e6e600"
-              className="w-full text-center text-black mt-20  "
+              className="w-full text-center text-black mt-10"
             />
           </div>
           <div
@@ -166,155 +167,117 @@ function Home() {
             data-aos="slide-right"
           >
             <FaHtml5
-              size={150}
+              size={120}
               color="#EC6231"
               className="w-full text-center mt-20  "
             />
             <FaCss3
-              size={150}
+              size={120}
               color="blue"
               className="w-full text-center mt-20    "
             />
 
             <SiJquery
-              size={150}
+              size={120}
               color="#00009d"
               className="w-full text-center mt-20 "
             />
 
             <SiGithub
-              size={150}
+              size={120}
               color="black"
               className="w-full text-center mt-20  "
             />
 
             <SiTailwindcss
-              size={150}
+              size={120}
               color="skyblue"
               className="w-full text-center mt-20  "
             />
 
             <SiJest
-              size={150}
+              size={120}
               color="#a52a2a"
               className="w-full text-center mt-20 "
             />
           </div>
         </div>
+      </div>
 
-        {/* {Javscript Buff} */}
-        <div className="my-20">
-          <div className="text-center h-52 bg-primary">
-            <h1 className="text-white font-bold text-4xl py-10">
-              My main is Javascript{" "}
-            </h1>
-          </div>
-
-          <div className="md:mx-5 mx-32 shadow-2xl bg-gray-50 -mt-20 rounded-lg hover:bg-gray-700 hover:text-white">
-            <div className="h-96" data-aos="zoom-in">
-              <lottie-player
-                src="https://assets7.lottiefiles.com/packages/lf20_bwlwtrx8.json"
-                background="transparent"
-                speed="1"
-                loop
-                autoplay
-              ></lottie-player>
-            </div>
-
-            <p className="text-xl my-5 font-semibold md:px-5 px-14 py-10">
-              JavaScript is one of the most top-ranked programming languages
-              because of its ubiquitous use on all platforms and mass adoption.
-              Main Use Cases: Web Development.
-            </p>
-          </div>
-        </div>
-
-        {/* {Tech stack section} */}
-        <div className="my-20">
-          <div className="text-center h-52 bg-red-500">
-            <h1 className="text-white font-bold text-4xl py-10">
-              My Tech Stack
-            </h1>
-          </div>
-
-          <div className="md:mx-5 mx-32 shadow-2xl bg-gray-50 -mt-20 rounded-lg hover:bg-gray-700 hover:text-white">
-            <div className="h-96" data-aos="zoom-in">
-              <lottie-player
-                src="https://assets2.lottiefiles.com/packages/lf20_aptscmnx.json"
-                background="transparent"
-                speed="1"
-                loop
-                autoplay
-              ></lottie-player>
-            </div>
-
-            <div className="grid md:grid-cols-1 grid-cols-3 p-5">
-              <div>
-                <h1 className="text-xl font-bold">Front End</h1>
-                <hr />
-                <p className="font-semibold mt-2">HTML/CSS</p>
-                <p className="font-semibold mt-2">React</p>
-
-                <p className="font-semibold mt-2">Javascript</p>
-              </div>
-
-              <div className="text-center">
-                <h1 className="text-xl font-bold">UI / UX</h1>
-                <hr />
-                <p className="font-semibold mt-2">Bootstrap</p>
-                <p className="font-semibold mt-2">Tailwind</p>
-
-                <p className="font-semibold mt-2">Ant Design</p>
-                <p className="font-semibold mt-2">Material UI</p>
-                <p className="font-semibold mt-2">Semantic UI</p>
-              </div>
-
-              <div className="text-right">
-                <h1 className="text-xl font-bold">Backend and DB</h1>
-                <hr />
-                <p className="font-semibold mt-2">Node JS</p>
-                <p className="font-semibold mt-2">Express JS</p>
-
-                <p className="font-semibold mt-2">Mongo DB</p>
-              </div>
-            </div>
-          </div>
-        </div>
-
-        {/* Dev Info */}
-        <div>
-          <h1 className="text-4xl text-gray-500 text-center font-bold">
-            Who is Ashvin ?
+      {/* Project */}
+      <div className="mt-20">
+        <div className="font-bold text-center bg-red-500 mx-20 p-2 text-white rounded-tl-full rounded-br-full md:mt-10 md:mx-5">
+          <h1 className="text-3xl md:text-3xl" data-aos="slide-left">
+            PROJECTS
           </h1>
+          <h1 className="text-3xl md:text-3xl" data-aos="slide-right">
+            Here you can find some of the projects I made so far
+          </h1>
+        </div>
+      </div>
 
-          <div className="h-screen relative text-gray-800">
-            <div className="h-full">
-              <lottie-player
-                src="https://assets5.lottiefiles.com/packages/lf20_1krz51xr.json"
-                background="transparent"
-                speed="1"
-                loop
-                autoplay
-              ></lottie-player>
+      <div className="grid mt-10 md:grid-cols-1 grid-cols-3 items-center justify-center gap-10 mx-20 md:mx-5">
+        {projectsData.map((project) => {
+          return (
+            <div>
+              <div className="relative p-10 border-2 text-center rounded-tr-3xl rounded-bl-3xl border-gray-400">
+                <img
+                  src={project.image}
+                  alt="project1"
+                  className="w-full h-52 text-center"
+                />
+
+                <div className="absolute inset-0 flex items-center justify-center flex-col opacity-0 bg-black hover:opacity-80 rounded-tr-3xl rounded-bl-3xl">
+                  <h1 className="text-4xl font-semibold text-white">
+                    {project.title}
+                  </h1>
+                  <button className="border-2 rounded border-white py-2 hover:bg-green-500 px-5 mt-5 text-white">
+                    DEMO
+                  </button>
+                </div>
+              </div>
             </div>
-            <div className="absolute inset-0 flex flex-col items-center justify-center">
-              <h1 className="text-2xl font-bold">
-                Hi , Hello , How are you?
-                <hr />
-                <pre className="text-xl md:text-sm my-5 font-quicksand font-semibold">
-                  {JSON.stringify(
-                    {
-                      name: "Ashvin Sunga",
-                      age: 28,
-                      gender: "Male",
-                      country: "Philippines",
-                    },
-                    null,
-                    2
-                  )}
-                </pre>
-              </h1>
-            </div>
+          );
+        })}
+      </div>
+
+      {/* Contact */}
+      <div className="grid grid-cols-2 md:grid-cols-1 ">
+        <div className="h-screen mt-10 md:mt-0">
+          <lottie-player
+            src="https://assets10.lottiefiles.com/packages/lf20_88z2psap.json"
+            background="transparent"
+            speed="0.5"
+            loop
+            autoplay
+          ></lottie-player>
+        </div>
+
+        <div className="w-screen mt-20 md:-mt-24 md:ml-7">
+          <div className="md:w-5/6 w-2/5 h-80% md:h-fit p-10 shadow-2xl bg-gray-50">
+            <h1 className="text-xl font-semibold">
+              Do you have any questions? You can email me here.
+            </h1>
+            <input
+              type="text"
+              placeholder="Name"
+              className="w-full border-2 border-gray-400 rounded p-1 shadow-lg mt-5"
+            />
+            <input
+              type="text"
+              placeholder="Email"
+              className="w-full border-2 border-gray-400 rounded p-1 shadow-lg mt-5"
+            />
+            <textarea
+              rows={5}
+              type="text"
+              placeholder="Message..."
+              className="w-full border-2 border-gray-400 rounded p-1 shadow-lg mt-5"
+            />
+
+            <button className="bg-red-500 rounded text-white px-5 py-1 mt-3">
+              SUBMIT
+            </button>
           </div>
         </div>
       </div>
